90ea7998592fffa5a81182a9076a9ae09415d2a1
manharr <142604277+manharr@users.noreply.github.com>
2025-02-15 13:15:32 +0530

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-      origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
 
 }));
 
@@ -34,9 +34,10 @@ app.get("/", (req, res)=>{
 
 app.use("/upload", uploadRouter);
 app.use("/uploads", express.static("uploads")); 
-app.listen(5000, () =>{
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000 hello')
-})
+    console.log(`Server running on port ${PORT}`);
+});
 
 // 
