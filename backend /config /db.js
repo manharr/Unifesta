fb6aed7f2fc948bee48b312272a5233a2eb5abd0
manharr <142604277+manharr@users.noreply.github.com>
2025-02-15 12:38:06 +0530

    Update db.js

--- backend /config /db.js
+++ backend /config /db.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+export const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected : ${conn.connection.host}`);
+        console.log(`Database Name: ${conn.connection.db.databaseName}`);
+
+    } catch(error){
+        console.error(`ERROR: ${error.message}`);
+        process.exit(1); // 1 means exit 0 means success
+
+    }
+};
