5408c6246b284f3854ec16a30e692c4f4a69dc87
manharr <142604277+manharr@users.noreply.github.com>
2025-02-15 12:38:48 +0530

    Delete backend/controllers directory

--- backend/controllers/upload-controller.js
+++
@@ -1,7 +0,0 @@
-export const uploadImage = async (req, res) => {
-    try {
-        res.status(201).json({ message: "Image uploaded successfully!", imageUrl: `/uploads/${req.file.filename}` });
-    } catch (err) {
-        res.status(500).json({ message: "Error uploading image", error: err.message });
-    }
-};
